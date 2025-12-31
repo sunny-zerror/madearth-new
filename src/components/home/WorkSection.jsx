0dc3e89d5d329d77cdcbcaa52065bd78d8cd165f
sunny-zerror <sunny.zerror@gmail.com>
2025-12-31 15:40:49 +0530

    work re arrangement

--- src/components/home/WorkSection.jsx
+++ src/components/home/WorkSection.jsx
@@ -73,8 +73,8 @@ const WorkSection = () => {
         cards.forEach((card) => {
             const randomMove = () => {
                 gsap.to(card, {
-                    x: gsap.utils.random(-50, 50),
-                    y: gsap.utils.random(-50, 50),
+                    x: gsap.utils.random(-20, 20),
+                    y: gsap.utils.random(-20, 20),
                     duration: gsap.utils.random(1, 2),
                     ease: "power1.inOut",
                     onComplete: randomMove,
@@ -85,14 +85,32 @@ const WorkSection = () => {
         });
     }, []);
 
+    const handleMouseEnter = () => {
+        gsap.to(".wrk_overlay", {
+                opacity:1,
+                ease:"expo.out",
+                // duration:.5,
+
+        })
+    } 
+    const handleMouseLeave = () => {
+        gsap.to(".wrk_overlay", {
+                opacity:0,
+                ease:"expo.out",
+                // duration:.5,
+        })
+    } 
+
 
 
     return (
         <>
             <div className=" wrk_paren relative  center flex-col w-full">
-                <h2 className=' sticky top-1/2 -translate-y-1/2 whitespace-nowrap  wrk_txt text-[20vw] uppercase'>  Our Work</h2>
+
+                <div className="wrk_overlay pointer-events-none opacity-0 top-0 z-[2] left-0 h-full w-full fixed bg-black/80 "></div>
+                <h2 className=' sticky z-[1] top-1/2 -translate-y-1/2 whitespace-nowrap  wrk_txt text-[20vw] uppercase'>  Our Work</h2>
                 <div className="w-full h-screen"></div>
-                <div className=" translate-y-[-12vw]  wrks_crds_pren ">
+                <div className=" translate-y-[-12vw] relative z-[10]  wrks_crds_pren ">
                     <div
                         ref={el => (cardsRef.current = el)}
                         className="w-full grid grid-cols-4 gap-10 items-center justify-center group"
@@ -100,7 +118,9 @@ const WorkSection = () => {
                         {WorksData.map((item, i) => (
                             <div
                                 key={i}
-                                className="w-[20vw] card cursor-pointer overflow-hidden relative transition duration-300 brightness-90  group-hover:brightness-50 hover:brightness-100">
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                                className="w-[20vw] z-[10] card cursor-pointer overflow-hidden relative transition-all duration-300 brightness-90  group-hover:brightness-[.3] hover:brightness-100 hover:text-white">
                                     
                                 <div className="crd_img scale-0 relative rounded-xl aspect-[3/4] w-full overflow-hidden">
                                     <video
@@ -112,18 +132,20 @@ const WorkSection = () => {
                                         playsInline
                                     ></video>
                                     <img
-                                        className="  cover   opacity-100   hover:opacity-0  transition duration-300  "
+                                        className="  cover opacity-100   hover:opacity-0  transition duration-300  "
                                         src={item.image}
                                         alt=""
                                     />
                                 </div>
+                                <div className="relative z-[40]">
                                 <h2 className="text-5xl mt-2 leading-none uppercase">{item.title}</h2>
                                 <p className="leading-none opacity-70">{item.subtitle}</p>
+                                </div>
                             </div>
                         ))}
                     </div>
 
-                    <div className="w-full center mt-10">
+                    <div className="w-full center relative mt-10">
                         <MagnetButton link={"/work"} text={"View All"} />
                     </div>
                 </div>
