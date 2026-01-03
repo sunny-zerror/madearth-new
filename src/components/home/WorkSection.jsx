0cc61eee732a31f60a96cf0646fc9dc6cb02b2b5
sunny-zerror <sunny.zerror@gmail.com>
2026-01-03 17:14:41 +0530

    work section animation redesign

--- src/components/home/WorkSection.jsx
+++ src/components/home/WorkSection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { WorksData } from '@/data/WorksData'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
@@ -11,7 +11,10 @@ gsap.registerPlugin(ScrollTrigger)
 
 
 const WorkSection = () => {
-    const cardsRef = useRef([]);
+
+    const [bg_data, setBg_data] = useState(null)
+    const [activeIndex, setActiveIndex] = useState(null);
+    const activeVideo = useRef(null);
 
     useGSAP(() => {
         gsap.from(".wrk_txt", {
@@ -30,7 +33,7 @@ const WorkSection = () => {
             scrollTrigger: {
                 trigger: ".wrk_paren",
                 start: "top top",
-                end: "bottom center",
+                end: "40% top",
                 scrub: true,
             },
         });
@@ -41,7 +44,7 @@ const WorkSection = () => {
 
     useEffect(() => {
         const elems = gsap.utils.toArray(".crd_img");
-        const order = [1, 3, 2, 0 ,4 ,6 , 5, 7];
+        const order = [1, 3, 2, 0, 4, 6, 5, 7];
         const seq = order.map(i => elems[i]).filter(Boolean);
 
         const tl = gsap.timeline({
@@ -73,8 +76,8 @@ const WorkSection = () => {
         cards.forEach((card) => {
             const randomMove = () => {
                 gsap.to(card, {
-                    x: gsap.utils.random(-20, 20),
-                    y: gsap.utils.random(-20, 20),
+                    x: gsap.utils.random(-10, 10),
+                    y: gsap.utils.random(-10, 10),
                     duration: gsap.utils.random(1, 2),
                     ease: "power1.inOut",
                     onComplete: randomMove,
@@ -85,67 +88,74 @@ const WorkSection = () => {
         });
     }, []);
 
-    const handleMouseEnter = () => {
-        gsap.to(".wrk_overlay", {
-                opacity:1,
-                ease:"expo.out",
-                // duration:.5,
+    const handleMouseEnter = (bg_video, index) => {
+        if (activeVideo.current === bg_video) return;
 
-        })
-    } 
+        activeVideo.current = bg_video;
+        setActiveIndex(index);
+        setBg_data(bg_video);
+
+        gsap.to(".bg_video_paren", { opacity: 1, duration: 0.5 });
+    };
+
     const handleMouseLeave = () => {
-        gsap.to(".wrk_overlay", {
-                opacity:0,
-                ease:"expo.out",
-                // duration:.5,
-        })
-    } 
+        activeVideo.current = null;
+        setActiveIndex(null);
 
+        gsap.to(".bg_video_paren", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: () => setBg_data(null),
+        });
+    };
 
 
     return (
         <>
             <div className=" wrk_paren relative  center flex-col w-full">
 
-                <div className="wrk_overlay pointer-events-none opacity-0 top-0 z-[2] left-0 h-full w-full fixed bg-black/80 "></div>
+                <div className=" bg_video_paren opacity-0  fixed top-0 left-0 w-full h-screen  z-[2] pointer-events-none">
+
+                    <video loop muted playsInline autoPlay className='cover brightness-[.9]' src={bg_data}></video>
+
+                </div>
                 <h2 className=' sticky z-[1] top-1/2 -translate-y-1/2 whitespace-nowrap  wrk_txt text-[20vw] uppercase'>  Our Work</h2>
                 <div className="w-full h-screen"></div>
                 <div className=" translate-y-[-12vw] relative z-[10]  wrks_crds_pren ">
                     <div
-                        ref={el => (cardsRef.current = el)}
-                        className="w-full grid grid-cols-4 gap-10 items-center justify-center group"
+                        className="w-full grid grid-cols-4 gap-10 items-center justify-center "
                     >
                         {WorksData.map((item, i) => (
                             <div
                                 key={i}
-                                onMouseEnter={handleMouseEnter}
+                                onMouseEnter={() => handleMouseEnter(item.show_reel, i)}
                                 onMouseLeave={handleMouseLeave}
-                                className="w-[20vw] z-[10] card cursor-pointer overflow-hidden relative transition-all duration-300 brightness-90  group-hover:brightness-[.3] hover:brightness-100 hover:text-white">
-                                    
-                                <div className="crd_img scale-0 relative rounded-xl aspect-[3/4] w-full overflow-hidden">
-                                    <video
-                                        src={item.show_reel}
-                                        className=" cover absolute top-0 left-0  opacity-0  group-hover:opacity-0  hover:opacity-100 transition duration-300 "
-                                        loop
-                                        autoPlay
-                                        muted
-                                        playsInline
-                                    ></video>
+                                className={`w-[20vw] z-[10] group card cursor-pointer overflow-hidden relative `}>
+                                <div className={`crd_img scale-0 relative rounded-xl aspect-[3/4] w-full overflow-hidden transition-all duration-300     ${activeIndex !== null && activeIndex !== i
+                                    ? "border border-white"
+                                    : "border border-transparent"
+                                    } `}>
+                                    {activeIndex === i && (
+                                        <div className="w-full h-full center z-10 flex-col text-center bg-white absolute top-0 left-0 opacity-0 group-hover:opacity-100 duration-300 transition-all ">
+                                            <h2 className='text-6xl'>{item.title}</h2>
+                                            <p>{item.subtitle}</p>
+                                        </div>
+                                    )}
                                     <img
-                                        className="  cover opacity-100   hover:opacity-0  transition duration-300  "
+                                        className={`cover transition-opacity duration-300 ${activeIndex !== null && activeIndex !== i ? "opacity-0" : "opacity-100"}`}
                                         src={item.image}
                                         alt=""
                                     />
                                 </div>
                                 <div className="relative z-[40]">
-                                <h2 className="text-5xl mt-2 leading-none uppercase">{item.title}</h2>
-                                <p className="leading-none opacity-70">{item.subtitle}</p>
+                                    <h2 className={`text-3xl mt-2 leading-none uppercase transition-all duration-300 ${activeIndex !== null && activeIndex !== i ? "opacity-0" : "opacity-100"} `}>{item.title}</h2>
+                                    {/* <p className="leading-none opacity-70">{item.subtitle}</p> */}
                                 </div>
                             </div>
                         ))}
                     </div>
 
-                    <div className="w-full center relative mt-10">
+                    <div className="w-full center relative mt-20">
                         <MagnetButton link={"/work"} text={"View All"} />
                     </div>
                 </div>
